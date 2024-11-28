06f0e38c455f2a21b1eafd3cde2a37a9f463214f
Caio Burton <101891262+CaioBurton@users.noreply.github.com>
2024-11-27 21:05:44 -0300

    populando o banco de dados corretamente

--- diabetichealth_backend/alimentos/models.py
+++ diabetichealth_backend/alimentos/models.py
@@ -1,13 +1,11 @@
 from django.db import models
 
 class Alimento(models.Model):
-    nome = models.CharField(max_length=100)
-    calorias = models.FloatField(null=True, blank=True)
-    carboidratos = models.FloatField(null=True, blank=True)
-    proteinas = models.FloatField(null=True, blank=True)
-    gorduras = models.FloatField(null=True, blank=True)
-    categoria = models.CharField(max_length=50, null=True, blank=True)
-    indice_glicemico = models.IntegerField(null=True, blank=True)
+    nome = models.CharField(max_length=255)
+    medida_usual = models.CharField(max_length=255)
+    g_ou_ml = models.FloatField()
+    carboidratos = models.FloatField()  # cho (g)
+    calorias = models.FloatField()  # calorias (kcal)
 
     def __str__(self):
         return self.nome
